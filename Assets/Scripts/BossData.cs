40816a9a5c4c4ddb4e37dcfc92d448c6e72870b1
nguyenhai1302 <yasuo2018123@gmail.com>
2025-08-20 20:13:29 +0700

    DataBoss

---
+++ Assets/Scripts/BossData.cs
@@ -0,0 +1,29 @@
+using System;
+using Unity.Mathematics;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Boss", menuName = "Game/Boss")]
+public class BossData : ScriptableObject
+{
+    [Header("Boss Data")]
+    public string nameBoss;
+    public Sprite icon;
+    //public GameObject prefabBoss3D;
+
+    [Header("Offensive Stats")]
+    [Range(0f, 1000f)]
+    public float health;                            //Máu
+    [Range(0f, 100f)]
+    public float speed;                            //Tốc độ di chuyển
+    [Range(0f, 100f)]
+    public float damageAtk;                        //Sát thương tấn công
+    [Range(0f, 10f)]
+    public float speedAtk;                         //Tốc độ tấn công
+    public void OnValidate()
+    {
+        health = Math.Max(0, health);
+        speed = Math.Max(0, speed);
+        damageAtk = Math.Max(0, damageAtk);
+        speedAtk = Math.Max(0, speedAtk);
+    }
+}
