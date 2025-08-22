8d359e9eebf113eba274870520c72b4e1d12a8e3
nguyenhai1302 <yasuo2018123@gmail.com>
2025-08-22 18:05:32 +0700

    UpSkill

---
+++ Assets/Scripts/SkillBoss.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public abstract class SkillBoss : ScriptableObject
+{
+    public string skillName;
+    public float cooldown = 5f;
+
+    public void StartSkill(BossManager boss)
+    {
+        boss.StartCoroutine(SkillRoutine(boss));
+    }
+
+    private IEnumerator SkillRoutine(BossManager boss)
+    {
+        yield return new WaitForSeconds(Random.Range(1f, 3f)); // delay khởi động ngẫu nhiên
+
+        while (true)
+        {
+            Activate(boss); // dùng skill
+            yield return new WaitForSeconds(cooldown); // chờ hồi chiêu
+        }
+    }
+
+    protected abstract void Activate(BossManager boss);
+}
