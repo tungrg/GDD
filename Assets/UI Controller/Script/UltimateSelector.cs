c25fe6ccbd186ba5c1375f1ddf400f708ebe0273
Rinko1112 <uongquoctrung0000@gmail.com>
2025-08-25 17:50:35 +0700

    Playercontroller

---
+++ Assets/UI Controller/Script/UltimateSelector.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UltimateSelector : MonoBehaviour
+{
+    [Header("UI Buttons Skill Icons")]
+    public Button[] ultiButtons; 
+    public Sprite[] ultiIcons;   
+
+    [Header("Ultimate UI Button")]
+    public Image ultimateButtonIcon;
+
+    [Header("Panel")]
+    public GameObject panel;
+
+    [Header("Reference")]
+    public UltimateButton ultimateButton;
+
+    void Start()
+    {
+        if (panel == null) panel = this.gameObject;
+
+        for (int i = 0; i < ultiButtons.Length; i++)
+        {
+            int index = i; 
+            if (ultiButtons[i] != null)
+                ultiButtons[i].onClick.AddListener(() => SelectUltimate(index));
+        }
+    }
+
+    public void SelectUltimate(int index)
+    {
+        if (index < 0 || index >= ultiIcons.Length) return;
+
+        if (ultimateButtonIcon != null)
+            ultimateButtonIcon.sprite = ultiIcons[index];
+
+        if (ultimateButton != null)
+            ultimateButton.SetUltimate(ultiIcons[index], index);
+
+        if (panel != null)
+            panel.SetActive(false);
+    }
+
+    public void OpenPanel()
+    {
+        if (panel != null)
+            panel.SetActive(true);
+    }
+}
