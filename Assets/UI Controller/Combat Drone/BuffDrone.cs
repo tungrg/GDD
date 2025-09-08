653607e084895cd19b2a3ae81d0746dff8254b0e
Rinko1112 <uongquoctrung0000@gmail.com>
2025-09-08 17:03:58 +0700

    UISkill

---
+++ Assets/UI Controller/Combat Drone/BuffDrone.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuffDrone : MonoBehaviour
+{
+    [Header("Buff Settings")]
+    [Tooltip("Thời gian hiệu lực buff (giây)")]
+    public float duration = 20f;
+
+    private PlayerMove playerMove;
+    private int extraDashes = 1;
+
+    void Start()
+    {
+        playerMove = FindAnyObjectByType<PlayerMove>();
+    }
+
+    public void Activate()
+    {
+        if (playerMove == null) return;
+        StartCoroutine(BuffLifeCycle());
+    }
+
+    private IEnumerator BuffLifeCycle()
+    {
+        playerMove.SetMaxDashCount(1 + extraDashes);
+
+        yield return new WaitForSeconds(duration);
+
+        playerMove.SetMaxDashCount(1);
+    }
+}
